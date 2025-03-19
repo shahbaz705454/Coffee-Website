62ce4d0e85245728e5afc7a81d04c70cfb5f417d
mohd shahbaz <81930428+shahbaz705454@users.noreply.github.com>
2025-03-19 16:00:14 +0530

    completed halp portions

---
+++ script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            sliderPerView:1
+        },
+        768:{
+            sliderPerView:2
+        },
+        1024:{
+            sliderPerView:3
+        },
+    }
+  
+    // And if we need scrollbar
+ 
+  });
